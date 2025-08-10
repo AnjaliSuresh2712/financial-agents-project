03852ac0a77332453fefe651c9ca42b6a4c8a320
AnjaliSuresh2712 <suresh.anj@northeastern.edu>
2025-08-10 19:33:42 -0400

    Update data_api.py

---
+++ data_api.py
@@ -0,0 +1,48 @@
+import requests
+
+def get_stock_prices(ticker: str, interval="day", interval_multiplier=1, start_date=None, end_date=None):
+    url = "https://api.financialdatasets.ai/prices/"
+    params = {
+        "ticker": ticker,
+        "interval": interval,
+        "interval_multiplier": interval_multiplier,
+        "start_date": start_date,
+        "end_date": end_date
+    }
+    params = {k: v for k, v in params.items() if v is not None}
+    r = requests.get(url, params=params)
+    if r.status_code == 200:
+        return r.json()
+    print("Error:", r.status_code, r.text)
+    return None
+
+def get_financial_metrics(ticker: str, period="annual"):
+    url = "https://api.financialdatasets.ai/financial-metrics"
+    params = {"ticker": ticker, "period": period}
+    r = requests.get(url, params=params)
+    return r.json()
+
+def get_line_items(ticker):
+    url = "https://api.financialdatasets.ai/financials/search/line-items"
+    params = {"ticker": ticker}
+    r = requests.get(url, params=params)
+    return r.json()
+
+def get_insider_trades(ticker):
+    url = "https://api.financialdatasets.ai/insider-trades"
+    params = {"ticker": ticker}
+    r = requests.get(url, params=params)
+    return r.json()
+
+def get_news(ticker):
+    url = "https://api.financialdatasets.ai/news/"
+    params = {"ticker": ticker}
+    r = requests.get(url, params=params)
+    return r.json()
+
+def get_company_facts(ticker):
+    url = "https://api.financialdatasets.ai/company/facts/"
+    params = {"ticker": ticker}
+    r = requests.get(url, params=params)
+    return r.json()
+  
